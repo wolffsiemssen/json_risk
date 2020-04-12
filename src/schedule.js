a2dcb6cfe595e93980591c020614bfc548717a51
wolffsiemssen <tws@wolffsiemssen.de>
2020-04-12 18:17:56 +0200

    fix typo in error message for schedule generation

--- src/schedule.js
+++ src/schedule.js
@@ -47,7 +47,7 @@
                         if (!(next_to_last_dt instanceof Date) && stub_end) throw new Error("schedule: if next to last date is not provided and stub in the end is specified, effective date must be provided");
                 }
                 if ((eff_dt instanceof Date && maturity<eff_dt) || (library.valuation_date instanceof Date && maturity < library.valuation_date)) 
-                        throw new Error("schedule: maturity is before valution or effective date.");
+                        throw new Error("schedule: maturity is before valuation date or effective date.");
                 if(typeof tenor !== "number")
                         throw new Error("schedule: tenor must be a nonnegative integer, e.g., 6 for semiannual schedule, 0 for zerobond/iam schedule");
                 if(tenor<0 || Math.floor(tenor) !== tenor)
