21c2a469f98fc3cce9f9140898ca05e7669f5081
tilwolff <tilwolff@yahoo.com>
2021-01-29 10:13:23 +0100

    Add download and conversion scripts to rorac example

--- examples/rorac/scripts/convert.sh
+++ examples/rorac/scripts/convert.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-echo "$0: To be implemented"
-touch $1
+###### Transformieren der Zinskurve in das csv-Format der JsonRisk Apps
+echo -n "ZINSKURVE" > $2
+cat $1 |grep 'Monatsendstand'| head -n 1 | tr -cd '1234567890,;\n' | sed 's/;;/;/g ; s/;$//g ; s/\,0/Y/g' >> $2
+
+cat $1 |grep -E '[0-9]{4}-[0-9]{2};' | tr -cd '1234567890,;\-\n' |tr ',' '.' | sed 's/;;/;/g ; s/;$//g' | awk 'BEGIN{FS=";";OFS=";"}{for(i=2;i<=NF;i++){$i/=100};print}' >> $2
+
+
--- examples/rorac/scripts/download.sh
+++ examples/rorac/scripts/download.sh
@@ -1,4 +1,30 @@
 #!/bin/sh
 
-echo "$0: To be implemented"
-touch $1
+CURVE_TSIDS="
+BBK01.WX0073
+BBK01.WX0074
+BBK01.WX0075
+BBK01.WX0076
+BBK01.WX0077
+BBK01.WX0078
+BBK01.WX0079
+BBK01.WX0080
+BBK01.WX0082
+BBK01.WX0084
+BBK01.WX0087
+BBK01.WX0092
+BBK01.WX0097
+BBK01.WX0202
+"
+
+##### Herunterladen der der csv-Dateien von der Seite der BuBa
+URL="https://www.bundesbank.de/statistic-rmi/StatisticDownload?"
+for tsId in $CURVE_TSIDS; do
+	URL="${URL}tsId=${tsId}&"
+done
+URL="${URL}mode=its&its_csvFormat=de&its_currency=default&its_dateFormat=default" # &its_from=$TIMESTAMP&its_to=$TIMESTAMP"
+wget -q -O "$1" $URL
+if [ $? -ne 0 ]; then
+	echo "ERROR: Could not download $1"
+	exit 1
+fi
