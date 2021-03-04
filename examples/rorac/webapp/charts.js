0a31b6a5694c479a0d0d1e7399ece5461a82cb1c
kst <kerstin.steinberg@wolffsiemssen.de>
2021-03-04 16:43:10 +0100

    rorac graphics

--- examples/rorac/webapp/charts.js
+++ examples/rorac/webapp/charts.js
@@ -90,15 +90,14 @@
 		    responsive: true
 	    }
     );
-    var update_chart_scenarios=function(portfolio, value_name){	
+    var update_chart_scenarios=function(portfolio, value_index){	
         chart_scenarios.data.labels=[];  
         chart_scenarios.data.labels=portfolio.dates;
-        if (!value_name) value_name="rorac";
-        var index_value=portfolio.values_names.indexOf(value_name);
-       	chart_scenarios.options.title.text="Performance der Subportfolien";
+        if (undefined===value_index) value_index=portfolio.values_names.indexOf('rorac');
+       	chart_scenarios.options.title.text="Performance " + portfolio.values_names[value_index] + " der Subportfolien";
         for (i=0;i<portfolio.subportfolio.length;i++){
         	chart_scenarios.data.datasets[i].label=portfolio.subportfolio[i]; 
-        	chart_scenarios.data.datasets[i].data=portfolio.values[i][index_value];
+        	chart_scenarios.data.datasets[i].data=portfolio.values[i][value_index];
         }
         chart_scenarios.update();
     }
--- examples/rorac/webapp/main.js
+++ examples/rorac/webapp/main.js
@@ -1,20 +1,55 @@
 
-var app = angular.module('riskapp', []); 
+var app = angular.module('roracapp', []); 
 
 app.controller('main_ctrl', ['$scope', function($scope) { 
-
-	$scope.portfolio=JSON.parse(JSON.stringify(test)); 
-	$scope.value_names={list: null, selection: null};;
+			$scope.rorac=JSON.parse(JSON.stringify(rorac)); 
+			$scope.value_names={list: null, selection: null};
+			
+			$scope.initial_data=function(){ 
+		   		$scope.value_names.list=$scope.rorac.values_names;
+		   		$scope.value_names.selection="rorac";
+		   		update_data($scope.value_names.selection);
+	    	}
+	    	
+	   		$scope.initial_data();	 
+	   	  		
+			$scope.update=function(){
+				update_data($scope.value_names.selection)				
+			}
+				
+			function update_data(value_name){
+				var index_value=$scope.rorac.values_names.indexOf($scope.value_names.selection);
+				update_chart_scenarios($scope.rorac,index_value);
+				update_table(index_value);
+			}
+					
+			function update_table(index_value){
+				var data_table=[];
+ 				for (i=0;i<$scope.rorac.dates.length;i++){
+ 				data_table[i]={};
+ 				data_table[i]['Datum']=$scope.rorac.dates[i];
+ 					for (k=0;k<$scope.rorac.subportfolio.length;k++){
+ 					data_table[i][$scope.rorac.subportfolio[k]]=$scope.rorac.values[k][index_value][i]; 					
+ 					}				
+ 				}
+				$("#table").bootstrapTable();
+				$("#table").bootstrapTable('destroy');
+    			$("#table thead tr").html('');    	
+    			var tr = $('<th data-field="Datum">'+ $scope.value_names.selection + '<br>Datum</th>');
+      				$("#table thead tr").append(tr);
+    			for (i=0;i<$scope.rorac.subportfolio.length;i++){
+      				var tr = $('<th data-field="' + $scope.rorac.subportfolio[i] +'">'+ $scope.rorac.subportfolio[i] +'</th>');
+      				$("#table thead tr").append(tr);
+    			};     
+     			$(function () {
+					$('#table').bootstrapTable({
+    					data: data_table,
+    					formatLoadingMessage: function() {
+      							return '';
+    						}
+					});
+				});
+			};
 	
-	$scope.load_all_data=function(){ 	
-		   update_chart_scenarios($scope.portfolio);
-		   $scope.value_names.list=$scope.portfolio.values_names;
-	    }
-	    
-	$scope.load_all_data();
-
-	$scope.update_chart=function(){
-		update_chart_scenarios($scope.portfolio,$scope.value_names.selection);
-	};   
 }]);
 
