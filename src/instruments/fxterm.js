4bcef12face126bb6624d7df5d8378e9dcf7eed1
tilwolff <tilwolff@yahoo.com>
2026-03-14 18:27:55 +0100

    add more consistency checks for fx term legs

--- src/instruments/fxterm.js
+++ src/instruments/fxterm.js
@@ -10,7 +10,7 @@
      * @param {obj} obj A plain object representing the instrument
      * @param {string} [obj.currency=""] the currency in which this instrument's value is represented
      * @param {number} [obj.quantity=1.0] the quantity with which the instrument's value is multiplied
-     * @param {array} [obj.legs=[]] the legs of this instrument. Must be one or two legs. If two legs are specified, they must have two different currencies. If no legs are specified, one single leg is generated from terms and conditions.
+     * @param {array} [obj.legs=[]] the legs of this instrument. Must be one or two legs. If two legs are specified, they must have two different currencies. Legs must have one or two payments of type NotionalPayment. If no legs are specified, one single leg is generated from terms and conditions.
      * @param {date} [obj.acquire_date=01.01.1900] the acquire date
      */
     constructor(obj) {
@@ -57,6 +57,21 @@
         this.legs[0].currency === this.legs[1].currency
       ) {
         throw new Error("FxTerm: Legs must have different currencies.");
+      }
+
+      const num_payments = this.legs[0].payments.length;
+      if (1 !== num_payments && 2 !== num_payments)
+        throw new Error("FXTerm: legs must contain one or two payments");
+
+      for (const leg of this.legs) {
+        if (leg.payments.length !== num_payments)
+          throw new Error(
+            "FXTerm: both legs must contain the same number of payments",
+          );
+        for (const p of leg.payments) {
+          if (p.constructor !== library.NotionalPayment)
+            throw new Error("FxTerm: legs can only contain notional payments");
+        }
       }
     }
   }
